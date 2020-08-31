b15d9b0e3cb1971719a459fcfad89811ad72d371
Hubert Thieriot <hubert@energyandcleanair.org>
2020-08-31 15:35:43 +0200

    -splitting in separate file

---
+++ extract.R
@@ -0,0 +1,21 @@
+extract_omi_data <- function(){
+
+  # Specific requirements to parallelize value extraction
+  require(rgdal) #install.packages("rgdal")
+  require(velox) #remotes::install_github("hunzikp/velox")
+  require(rgis) #remotes::install_github("Pakillo/rgis")
+
+  print("Creating OMI value files. Can take a few hours if not days...")
+  radii_km <- c(2, 20, 200)
+  d.measures <- utils.read_points()
+
+  pts <- do.call("rbind", lapply(radii_km, utils.buffer, points=d.measures.wide))
+
+  # Get values
+  files <- list.files("data",pattern="\\.nc4$", full.names = T)
+
+  d <- do.call("rbind", pbapply::pblapply(files, utils.values_at_point, points=pts))
+  d.omi <- d %>% dplyr::select(NUMBER, radius_km, value, date)
+
+  saveRDS(d.omi, file_values)
+}
