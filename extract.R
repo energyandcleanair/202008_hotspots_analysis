da4d678d160da841f5497b6e95e8625bd20a56b8
Hubert Thieriot <hubert@energyandcleanair.org>
2020-09-02 15:03:18 +0200

    -extract only august 2020

--- extract.R
+++ extract.R
@@ -12,10 +12,10 @@ extract_omi_data <- function(){
   pts <- do.call("rbind", lapply(radii_km, utils.buffer, points=d.measures.wide))
 
   # Get values
-  files <- list.files("data",pattern="\\.nc4$", full.names = T)
+  files <- list.files("data",pattern="OMI-Aura_L3-OMSO2e_2020m08.*\\.nc4$", full.names = T)
 
   d <- do.call("rbind", pbapply::pblapply(files, utils.values_at_point, points=pts))
   d.omi <- d %>% dplyr::select(NUMBER, radius_km, value, date)
 
-  saveRDS(d.omi, file_values)
+  saveRDS(d.omi, "value_202008.RDS")
 }
