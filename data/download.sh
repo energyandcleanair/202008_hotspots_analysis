81162f834d3a3d11d28fc88dcd5686f937b954c1
Hubert Thieriot <hubert@energyandcleanair.org>
2020-09-22 14:21:56 +0200

    -adding new download files with solar zenith angle and cloud fraction

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,1 @@
-wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i SSW_download_2020-08-13T17_03_38_1262_s5HdqB4R.inp.txt
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i SSW_download_2020-09-22T12_19_47_3762_xeyGeGVP.inp
