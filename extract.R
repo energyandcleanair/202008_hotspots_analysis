c0339b6118afdcc0c00120c9551e96ce1f5b264e
Hubert Thieriot <hubert@energyandcleanair.org>
2020-09-25 08:53:23 +0800

    -extract all dates but 50km only

--- extract.R
+++ extract.R
@@ -7,16 +7,16 @@ extract_omi_data <- function(){
   require(dplyr)
 
   print("Creating OMI value files. Can take a few hours if not days...")
-  radii_km <- c(2, 20, 200)
+  radii_km <- c(50)
   d.measures.wide <- utils.read_points()
 
   pts <- do.call("rbind", lapply(radii_km, utils.buffer, points=d.measures.wide))
 
   # Get values
-  files <- list.files("data",pattern="OMI-Aura_L3-OMSO2e_2020m08.*\\.nc4$", full.names = T)
+  files <- list.files("data",pattern="OMI-Aura_L3-OMSO2e_20.*\\.nc4$", full.names = T)
 
   d <- do.call("rbind", pbapply::pblapply(files, utils.values_at_point, points=pts))
   d.omi <- d %>% dplyr::select(NUMBER, radius_km, value, date)
 
-  saveRDS(d.omi, "values_202008.RDS")
+  saveRDS(d.omi, "values_50km.RDS")
 }
